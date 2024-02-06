65f724a04cff41ba427b43d2a3ad9b31d794748c
Undral <Undral@LAPTOP-EUS26D6M>
2024-02-06 12:25:52 +0800

    1st

---
+++ src/aa/Circle.java
@@ -0,0 +1,10 @@
+package aa;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Undral
+ *
+ */
+module aa {
+}
